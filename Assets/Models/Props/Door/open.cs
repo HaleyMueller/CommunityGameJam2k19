b7aad7143be7622e45b88bcac6149e665a66d032
GuyGershon <GuyGershon@users.noreply.github.com>
2019-08-26 20:45:39 +0300

    Door + Player = D.O.N.E
    Door + Player = D.O.N.E

---
+++ Assets/Models/Props/Door/open.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class open : MonoBehaviour
+{
+    bool isInTheZone = false, isTheDoorOpen = false;
+    public GameObject Door, CanvasE, rotator;
+    public Animator anim;
+
+
+    
+    void Update()
+    {
+        if (isInTheZone)
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                if (!isTheDoorOpen)
+                {
+                    anim.Play("opening");
+                    print(Door.name + ("is open"));
+                    isTheDoorOpen = true;
+                    CanvasE.GetComponent<Canvas>().enabled = false;
+                }
+            }
+
+
+
+        }
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            print("the player entered the door zone");
+            isInTheZone = true;
+            
+            if (!isTheDoorOpen)
+            {
+                CanvasE.GetComponent<Canvas>().enabled = true;
+            }
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            print("the player left the door zone");
+            isInTheZone = false;
+            CanvasE.GetComponent<Canvas>().enabled = false;
+        }
+    }
+}
