62ec0c5f33500cdd5e904f2165f267f1a7963330
Mark Mueller <1998markmueller@gmail.com>
2019-08-27 18:54:17 -0500

    merging

---
+++ Assets/NPCs/Guard/Guard.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guard : WalkRoutine
+{
+    /// <summary>
+    /// Courtine that looks at the player and does a timer
+    /// </summary>
+    IEnumerator lookAtPlayer = null;
+
+    /// <summary>
+    /// Timer for how long player has to stay in view to be noticed
+    /// </summary>
+    public float noticeTimer = 4f;
+
+    [HideInInspector]
+    public bool doSpottedObject = false;
+
+    [HideInInspector]
+    public GameObject spottedObject = null;
+
+    public GameObject exclamation;
+
+    bool rayCastWorked = false;
+
+    void Update()
+    {
+        if (currentlyLookingAtPlayer && spottedObject == null && playerLeftView == false) //Player left view before timer ended
+        {
+            playerLeftView = true;
+            currentlyLookingAtPlayer = false;
+
+            if (lookAtPlayer != null)
+                StopCoroutine(lookAtPlayer);
+        }
+
+        if (doSpottedObject)
+        {
+            if (spottedObject != null) //Spotted object by trigger
+            {
+                float maxRange = 5;
+                RaycastHit hit;
+
+                if (Vector3.Distance(transform.position, spottedObject.transform.position) < maxRange) //Within raycast range
+                {
+                    if (Physics.Raycast(transform.position, (spottedObject.transform.position - transform.position), out hit, maxRange)) //If hit something
+                    {
+                        if (hit.transform.gameObject == spottedObject) //If hit spotted object directly
+                        {
+                            rayCastWorked = true;
+                            playerLeftView = false;
+
+                            var vec = new Vector3(spottedObject.transform.position.x, this.transform.position.y, spottedObject.transform.position.z);
+                            transform.LookAt(vec);
+
+                            if (currentlyLookingAtPlayer == false)
+                            {
+                                lookAtPlayer = NoticeTimer(); //Look at player and start timer
+                                StartCoroutine(lookAtPlayer);
+                            }
+                        }
+                        else
+                        {
+                            doSpottedObject = false;
+                            rayCastWorked = false;
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            PatrolLogic();
+        }
+    }
+
+    #region PlayerLookAt
+
+    private bool currentlyLookingAtPlayer = false;
+    private bool playerLeftView = false;
+
+    IEnumerator NoticeTimer()
+    {
+        Debug.Log("starting timer");
+        currentlyLookingAtPlayer = true;
+        yield return new WaitForSeconds(noticeTimer);
+        if (spottedObject != null)
+        {
+            Debug.Log("Successfully found player");
+            exclamation.GetComponent<SpriteRenderer>().enabled = true;
+            exclamation.GetComponent<AlwaysLookAtCamera>().enabled = true;
+        }
+        else
+        {
+            Debug.Log("It must have been the wind");
+        }
+        currentlyLookingAtPlayer = false;
+    }
+
+    #endregion
+}
---
+++ Assets/Player/PMC.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PMC : MonoBehaviour
+{
+    public float Speed;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift)){
+            float Horizontal = Input.GetAxis("Horizontal");
+            float Vertical = Input.GetAxis("Vertical");
+
+            Vector3 playerMovement = new Vector3 (Horizontal, 0f, Vertical) * Speed * 1.5f * Time.deltaTime;   
+            transform.Translate(playerMovement, Space.Self); 
+        }       
+        else
+        {
+            float Horizontal = Input.GetAxis("Horizontal");
+            float Vertical = Input.GetAxis("Vertical");
+
+            Vector3 playerMovement = new Vector3 (Horizontal, 0f, Vertical) * Speed * Time.deltaTime;   
+            transform.Translate(playerMovement, Space.Self); 
+        }
+    }
+}
