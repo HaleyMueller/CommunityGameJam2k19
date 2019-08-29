e9e990257228ecb8a112bb9a336ed9ab0ccce530
Mark Mueller <1998markmueller@gmail.com>
2019-08-28 19:29:06 -0500

    Added camera handler

---
+++ Assets/Player/CameraHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class CameraHandler : MonoBehaviour
+{
+    bool isInCameraMode = false;
+
+    public Camera c;
+
+    private void ShowLayer()
+    {
+        c.cullingMask |= 1 << LayerMask.NameToLayer("Camera");
+        isInCameraMode = true;
+    }
+
+    private void HideLayer()
+    {
+        c.cullingMask &= ~(1 << LayerMask.NameToLayer("Camera"));
+        isInCameraMode = false;
+    }
+    public Texture GetScreenShot()
+    {
+        return ScreenCapture.CaptureScreenshotAsTexture();
+    }
+}
