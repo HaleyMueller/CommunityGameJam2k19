98f6d5e02658dbddbecd62a5125fc549801f8e6e
Mark Mueller <1998markmueller@gmail.com>
2019-08-29 19:06:18 -0500

    Added camera detection if see monster

--- Assets/Player/CameraHandler.cs
+++ Assets/Player/CameraHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
@@ -8,7 +9,42 @@ public class CameraHandler : MonoBehaviour
 {
     bool isInCameraMode = false;
 
+    List<Picture> pictures = new List<Picture>();
+
+    public Renderer boss;
+    public RawImage test;
+
     public Camera c;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F)) //Toggles show camera UI
+        {
+            if (!isInCameraMode)
+            {
+                ShowLayer();
+            }
+            else
+            {
+                HideLayer();
+            }
+        }
+
+        if (isInCameraMode)
+        {
+            if (Input.GetKeyDown(KeyCode.T)) //Take picture
+            {
+                var bossVisible = boss.isVisible;
+
+                SavePicture(bossVisible, false); //NOTE: if you can see the boss in the editor window the value will be set to true
+
+                if (bossVisible) //Took picture of boss. Player now has to leave level
+                {
+
+                }
+            }
+        }
+    }
 
     private void ShowLayer()
     {
@@ -21,8 +57,34 @@ public class CameraHandler : MonoBehaviour
         c.cullingMask &= ~(1 << LayerMask.NameToLayer("Camera"));
         isInCameraMode = false;
     }
-    public Texture GetScreenShot()
+
+    public Picture SavePicture(bool isMonsterInView, bool isDeath)
     {
-        return ScreenCapture.CaptureScreenshotAsTexture();
+        var t = ScreenCapture.CaptureScreenshotAsTexture();
+
+        Picture p = new Picture();
+        p.picture = t;
+        p.isMonster = isMonsterInView;
+        p.isDeath = isDeath;
+
+        pictures.Add(p);
+        return p;
+    }
+
+    public Picture GetPictureWithMonster() //Raw Image ui texture is good to picture display on
+    {
+        return pictures.Where(x => x.isMonster == true).LastOrDefault();
+    }
+
+    public Picture GetPictureWithDeath()
+    {
+        return pictures.Where(x => x.isDeath == true).LastOrDefault();
+    }
+
+    public class Picture
+    {
+        public Texture picture;
+        public bool isMonster = false;
+        public bool isDeath = false;
     }
 }
--- Assets/Player/PMC.cs
+++ Assets/Player/PMC.cs
@@ -8,20 +8,24 @@ public class PMC : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftShift)){
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
             float Horizontal = Input.GetAxis("Horizontal");
             float Vertical = Input.GetAxis("Vertical");
 
-            Vector3 playerMovement = new Vector3 (Horizontal, 0f, Vertical) * Speed * 1.5f * Time.deltaTime;   
-            transform.Translate(playerMovement, Space.Self); 
-        }       
+            Vector3 playerMovement = new Vector3(Horizontal, 0f, Vertical) * Speed * 1.5f * Time.deltaTime;
+            transform.Translate(playerMovement, Space.Self);
+        }
         else
         {
             float Horizontal = Input.GetAxis("Horizontal");
             float Vertical = Input.GetAxis("Vertical");
 
-            Vector3 playerMovement = new Vector3 (Horizontal, 0f, Vertical) * Speed * Time.deltaTime;   
-            transform.Translate(playerMovement, Space.Self); 
+            if (Horizontal != 0 || Vertical != 0)
+            {
+                Vector3 playerMovement = new Vector3(Horizontal, 0f, Vertical) * Speed * Time.deltaTime;
+                transform.Translate(playerMovement, Space.Self);
+            }
         }
     }
 }
