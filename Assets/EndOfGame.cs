998f4cbbec4492e867f95895cc9779c8212680f0
Mark Mueller <1998markmueller@gmail.com>
2019-08-31 12:49:44 -0500

    work

---
+++ Assets/EndOfGame.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndOfGame : MonoBehaviour
+{
+
+    bool playerin = false;
+
+    public GameObject canvas;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerin = true;
+
+            var play = GameObject.Find("Player");
+
+            if (play.GetComponent<CameraHandler>().GetPictureWithMonster() != null)
+            {
+                ImageHolder.image = GameObject.FindGameObjectWithTag("Player").GetComponent<CameraHandler>().GetPictureWithMonster().picture;
+
+                SceneManager.LoadScene("Won");
+            }
+            else
+            {
+                canvas.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerin = false;
+
+            canvas.SetActive(false);
+        }
+    }
+
+
+}
