7076ffe6dfe4167ca5a846b61a374504fae0f0b2
AlbertoQP <alberto.quesada.pablos@gmail.com>
2019-08-31 17:21:06 +0200

    Level Finished

---
+++ Assets/BossDoors.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossDoors : MonoBehaviour
+{
+	public GameObject doors;
+
+    
+    void Start()
+    {
+		doors.gameObject.SetActive(false);
+    }
+
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.CompareTag("Player"))
+		{
+			doors.SetActive(true);
+			Destroy(gameObject);
+		}
+	}
+}
